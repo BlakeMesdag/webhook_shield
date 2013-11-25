3aa59e72c53f69971cb8e087104f0bb8b355e0c6
Blake Mesdag <blakemesdag@gmail.com>
2013-11-24 21:31:44 -0500

    Wtf, kensa doesn't allow underscore

--- app/controllers/heroku/resources_controller.rb
+++ app/controllers/heroku/resources_controller.rb
@@ -26,7 +26,7 @@ class Heroku::ResourcesController < ApplicationController
 
     render json: {
       id: resource.id,
-      config: {'WEBHOOK_SHIELD_URL' => resource.webhook_url},
+      config: {'WEBHOOKSHIELD_URL' => resource.webhook_url},
       message: "You're all setup to accept webooks at ..."
     }
   end
